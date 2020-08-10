f05e9945ea94ba9d58858d9f62df0d12c3ba84a0
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-10 23:43:30 +0530

    [Ananth] Adds response controller

---
+++ app/Http/Controllers/responseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class ResponseController extends Controller
+{
+    public static function Response200($message, $data = null)
+    {
+        $responseJsonData = array();
+        $responseJsonData['status'] = Config::get('constants.response.success');
+        if ($message) {
+            $responseJsonData['message'] = $message;
+        }
+        if ($data) {
+            $responseJsonData['data'] = $data;
+        }
+        return response()->json($responseJsonData, 200);
+    }
+
+    public static function Error422($message, $data = null)
+    {
+        $responseJsonData = array();
+        $responseJsonData['status'] = Config::get('constants.response.error');
+        if ($message) {
+            $responseJsonData['message'] = $message;
+        }
+        if ($data) {
+            $responseJsonData['data'] = $data;
+        }
+        return response()->json($responseJsonData, 422);
+    }
+}
