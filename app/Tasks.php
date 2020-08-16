1a15f73d6ddd55d26fa00ce8a03dd21f9f6d5679
Ananth Sekar <ananth.sekar@gogsoon.com>
2020-08-16 16:58:34 +0530

    [Ananth] Adds fillables in Tasks model

--- app/Tasks.php
+++ app/Tasks.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tasks extends Model
 {
-    protected $fillable = ['title', 'description', 'user_id'];
+    protected $fillable = ['title', 'description', 'user_id', 'isCompleted', 'isArchieved'];
     protected $primaryKey = 'task_id';
     protected $table = 'tasks';
 }
